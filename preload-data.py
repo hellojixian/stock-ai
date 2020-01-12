61824bea76bd8feedfeed6f447a01ccfb0db3ab9
Jixian Wang <hellojixian@gmail.com>
2020-01-12 23:35:52 +0800

    update

--- preload-data.py
+++ preload-data.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import pandas as pd
-
+import warnings
 from lib.datasource import DataSource as ds
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # set output
 pd.set_option('display.max_rows', 500)
