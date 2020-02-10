c259d07020d472a2e4809a50e9dda0418ad695c2
Jixian Wang <hellojixian@gmail.com>
2020-02-10 20:49:23 +0800

    update

--- lib/indicators/kdj.py
+++ lib/indicators/kdj.py
@@ -7,7 +7,8 @@ class KDJ(BaseStrategy):
     FEATURES = [
         ['kdj_j_scaled',  -0.5,  2],
         ['kdj_j_diff',       0,  3],
-        ['kdj_j_diff_prev',  0,  3],        
+        ['kdj_j_diff_prev',  0,  3],
+        ['kdj_j_bias',       0,  0.02],
         ['change',      0,  10],
         ['amp_0105',    0,  2],
         ['amp_0510',    0,  1],
