adf7da05d1acdd5b208aed74bb2a796945fe277f
Jixian Wang <hellojixian@gmail.com>
2020-02-10 20:26:13 +0800

    update

--- lib/indicators/__init__.py
+++ lib/indicators/__init__.py
@@ -2,9 +2,11 @@ from .dropdays import DropDays
 from .trend import Trend
 from .rsi import RSI
 from .ma import MA
+from .ma import KDJ
 
 indicators = {
     'rsi':RSI,
+    'kdj':KDJ,
     'ma':MA,
     'trend':Trend,
     'dropdays':DropDays
