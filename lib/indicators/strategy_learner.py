9422055433b8e4021d67457b8086c28564c9d7dc
Jixian Wang <hellojixian@gmail.com>
2020-02-13 01:21:17 +0800

    update

--- lib/indicators/strategy_learner.py
+++ lib/indicators/strategy_learner.py
@@ -144,7 +144,7 @@ class StrategyLearner(object):
     def _loss_function(self, report):
         # if report['sess']==0: return 0
         # score = (report['profit'] - report['baseline']) * report['win_r'] / (report['cont_errs'] +1)
-        score = report['win_r'] / (report['cont_errs']**2 +1)
+        score = report['win_r'] * 1/(report['cont_errs'] +1)
         return score
 
     def evaluate_dna(self, DNA, datasource=None):
