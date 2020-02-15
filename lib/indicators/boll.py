b01c0256bf5281a1c09467ce4d911acdaef0d4ea
Jixian Wang <hellojixian@gmail.com>
2020-02-16 02:48:40 +0800

    update

--- lib/indicators/boll.py
+++ lib/indicators/boll.py
@@ -8,7 +8,7 @@ class BOLL(BaseStrategy):
         ['bb_pos',      -0.5,     1],
         ['bb_diff',        0,   100],
         ['bb_diff_prev',   0,   100],
-        ['bb_scope',       0,     1],
+        ['bb_scope',       0,    20],
         ['change',         0,    10],
         ['amp_0105',       0,     2],
         ['amp_0510',       0,     1],
