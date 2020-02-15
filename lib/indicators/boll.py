205c7458b9c688740e16c5bc503f930543c5cc27
Jixian Wang <hellojixian@gmail.com>
2020-02-16 02:31:18 +0800

    update

--- lib/indicators/boll.py
+++ lib/indicators/boll.py
@@ -5,10 +5,10 @@ class BOLL(BaseStrategy):
     NAME = 'boll'
     # Feature,   Bias,   Scaler
     FEATURES = [
-        ['bb_pos',      -0.5,     2],
+        ['bb_pos',      -0.5,     1],
         ['bb_diff',        0,   100],
         ['bb_diff_prev',   0,   100],
-        ['bb_scope',       0,     2],
+        ['bb_scope',       0,     1],
         ['change',         0,    10],
         ['amp_0105',       0,     2],
         ['amp_0510',       0,     1],
