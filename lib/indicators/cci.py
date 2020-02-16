56e061e8ce21e6fe0614e57fe78c705c9bc3501d
Jixian Wang <hellojixian@gmail.com>
2020-02-16 15:48:18 +0800

    update

--- lib/indicators/cci.py
+++ lib/indicators/cci.py
@@ -5,15 +5,15 @@ class CCI(BaseStrategy):
     NAME = 'cci'
     # Feature,   Bias,   Scaler
     FEATURES = [
-        ['cci_bias',      0,   100],
-        ['cci_7',         0,   100],
-        ['cci_7_diff',    0,   100],
-        ['cci_14',        0,   100],
-        ['cci_14_diff',   0,   100],
-        ['cci_25',        0,     2],
-        ['change',        0,    10],
-        ['amp_0105',      0,     2],
-        ['amp_0510',      0,     1],
+        ['cci_bias',      0,   0.01],
+        ['cci_7',         0,   0.01],
+        ['cci_7_diff',    0,   0.01],
+        ['cci_14',        0,   0.01],
+        ['cci_14_diff',   0,   0.01],
+        ['cci_25',        0,   0.01],
+        ['change',        0,     10],
+        ['amp_0105',      0,      2],
+        ['amp_0510',      0,      1],
     ]
     DNA_LEN = len(FEATURES)*2
 
